8c96d4c9d95c3754af37ea792102333e707af53b
Till Hein <till.hein@dg-i.net>
2019-11-22 11:17:22 +0100

    Einfache Darstellung eines Dementiell-Erkrankten

--- src/main/java/DementiellErkrankter/DementiellErkrankter.java
+++ src/main/java/DementiellErkrankter/DementiellErkrankter.java
@@ -1,4 +1,25 @@
 package DementiellErkrankter;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class DementiellErkrankter {
+
+    private UUID id;
+    private String name;
+    private String vorname;
+    private Boolean zustimmung;
+    private Integer schuhgroesse;
+
+    public DementiellErkrankter() {
+        this.id = UUID.randomUUID();
+    }
 }
