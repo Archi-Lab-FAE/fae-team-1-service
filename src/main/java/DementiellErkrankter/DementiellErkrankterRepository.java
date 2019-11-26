5e3918b735fac9d3e08bbe40233b24cd7123c24d
Till Hein <till.hein@dg-i.net>
2019-11-26 16:53:57 +0100

    fehlerhafte Klassen löschen

--- src/main/java/DementiellErkrankter/DementiellErkrankterRepository.java
+++
@@ -1,8 +0,0 @@
-package DementiellErkrankter;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface DementiellErkrankterRepository extends CrudRepository <DementiellErkrankter, UUID> {
-}
