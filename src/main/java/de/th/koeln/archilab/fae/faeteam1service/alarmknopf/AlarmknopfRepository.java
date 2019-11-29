7e4288a219c77ce5fb20cfd7c4d6a346d849bd2d
Stephan Hagge <Stephan.Hagge@aalberts-mt.com>
2019-11-29 13:38:13 +0100

    Tschüss

--- src/main/java/de/th/koeln/archilab/fae/faeteam1service/alarmknopf/AlarmknopfRepository.java
+++
@@ -1,8 +0,0 @@
-package de.th.koeln.archilab.fae.faeteam1service.alarmknopf;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AlarmknopfRepository extends JpaRepository<Alarmknopf, UUID> {
-}
