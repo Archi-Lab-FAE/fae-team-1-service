8f90bdb0fb11005423926e7c20721ad9829d85dc
Stephan Hagge <Stephan.Hagge@aalberts-mt.com>
2020-01-08 11:21:52 +0100

    Test correction

--- src/test/java/de/th/koeln/archilab/fae/faeteam1service/demenziellErkrankter/DemenziellErkrankterTest.java
+++ src/test/java/de/th/koeln/archilab/fae/faeteam1service/demenziellErkrankter/DemenziellErkrankterTest.java
@@ -52,6 +52,6 @@ public class DemenziellErkrankterTest {
                 .andExpect(jsonPath("$[0].id").value(demenziellErkrankter.getId().toString()))
                 .andExpect(jsonPath("$[0].name").isEmpty())
                 .andExpect(jsonPath("$[0].vorname").isEmpty())
-                .andExpect(jsonPath("$[0].schuhgroesse").isEmpty());
+                .andExpect(jsonPath("$[0].zustimmung").isEmpty());
     }
 }
